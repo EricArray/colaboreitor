81a59bede37cf4ec6558bbbdb5b403ccae23ac7d
Eric <arroyoericdamian@gmail.com>
2018-07-11 01:43:56 -0300

    org.postgresql.util.PSQLException

--- src/main/java/ar/edu/undav/colaboreitor/App.java
+++ src/main/java/ar/edu/undav/colaboreitor/App.java
@@ -1,12 +1,17 @@
 package ar.edu.undav.colaboreitor;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+	    //Class.forName("org.postgresql.Driver");
+	    DriverManager.registerDriver(new org.postgresql.Driver());
 		SpringApplication.run(App.class, args);
 	}
 
